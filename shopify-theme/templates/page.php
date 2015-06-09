a3e2e76b774b99039c158b262ad7d7808acc3be3
Craig Fowler <craig@wearegiants.com>
2015-06-09 15:16:16 -0700

    forget how command line works

--- shopify-theme/templates/page.php
+++ shopify-theme/templates/page.php
@@ -1,33 +1,5 @@
 <section id="page">
 
-  <?php
-
-  if (isset($_GET['index'])) {
-
-    include ('../templates/page.default.php');
-
-  } elseif (isset($_GET['about-us'])) {
-
-    include ('../templates/page.about-us.php');
-
-  } elseif (isset($_GET['find-us'])) {
-
-    include ('../templates/page.find-us.php');
-
-  } elseif (isset($_GET['contact-us'])) {
-
-    include ('../templates/page.contact-us.php');
-
-  } elseif (isset($_GET['faq'])) {
-
-    include ('../templates/page.faq.php');
-
-  } else {
-
-    include ('../templates/page.default.php');
-
-  }
-
-  ?>
+<?php include ('../../stage/content.page.php'); ?>
 
 </section>
---
+++ stage/content.page.php
@@ -0,0 +1,29 @@
+<?php
+
+if (isset($_GET['index'])) {
+
+  include ('../templates/page.default.php');
+
+} elseif (isset($_GET['about-us'])) {
+
+  include ('../templates/page.about-us.php');
+
+} elseif (isset($_GET['find-us'])) {
+
+  include ('../templates/page.find-us.php');
+
+} elseif (isset($_GET['contact-us'])) {
+
+  include ('../templates/page.contact-us.php');
+
+} elseif (isset($_GET['faq'])) {
+
+  include ('../templates/page.faq.php');
+
+} else {
+
+  include ('../templates/page.default.php');
+
+}
+
+?>
