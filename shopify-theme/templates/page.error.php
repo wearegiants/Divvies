53d71f89d8ac5af3821b3fb80a1805d215202c42
Craig Fowler <craig@wearegiants.com>
2015-08-05 11:49:26 -0700

    cart updated

--- shopify-theme/templates/page.error.php
+++ shopify-theme/templates/page.error.php
@@ -1,8 +1,8 @@
 <div id="error">
    <div id="error-centered">
       <div class="fs-row">
-         <div class="fs-cell fs-lg-8 fs-md-4 fs-sm-3 fs-centered">            
-            <img src="{{ 'error_img.png'  | asset_url }}" class="img-responsive" alt="Error" />
+         <div class="fs-cell fs-lg-8 fs-md-4 fs-sm-3 fs-centered">
+            <img src="/divvies-42418307/assets/error_img.png" class="img-responsive" alt="Error" />
             <br>
             <h1>Cookies Not Found</h1>
             <a href="/collections">Return to the Shop</a>
@@ -10,4 +10,3 @@
       </div>
    </div>
 </div>
-<hr class="invisible">
