d4bacaf5fca4e9c7005c5e79edd8089febe3eecc
Craig Fowler <craig@wearegiants.com>
2015-08-19 19:18:31 -0700

    things

--- shopify-theme/snippets/mailchimp.php
+++ shopify-theme/snippets/mailchimp.php
@@ -1,26 +1,3 @@
 <div id="mailchimp">
-   <button class="btn toggle" data-swap-target=".toggle_target_6" data-swap-linked="toggle_linked" data-swap-group="toggle_group_3">Sign up for the Newsletter</button>
+   <button class="btn toggle toggle_linkage" data-swap-target=".email-popup" data-swap-linked="toggle_linkage">Sign up for the Newsletter</button>
 </div>
-
-<div id="mailchimp-popup" class="toggle_target_6">
-   <div id="mailchimp-popup-wrapper" class="fs-cell fs-xl-5 fs-lg-6 fs-md-4 fs-sm-2 fs-contained">
-      <a href="#" id="mailchimp-close" class="toggle ss-gizmo ss-plus rotate-45" data-swap-target=".toggle_target_6" data-swap-linked="toggle_linked" data-swap-group="toggle_group_3"></a>
-      <div id="mc_embed_signup">
-         <form action="//divvies.us1.list-manage.com/subscribe/post?u=9526e58f22c750b09cd234943&amp;id=467716618f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-               <label for="mce-EMAIL">Subscribe to our mailing list</label>
-               <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-               <div style="position: absolute; left: -5000px;"><input type="text" name="b_9526e58f22c750b09cd234943_467716618f" tabindex="-1" value=""></div>
-               <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn bg-color-orange">
-            </div>
-         </form>
-      </div>
-   </div>
-</div>
-
-<?php 
-
-// User:  divvies
-// Pass:  metheny7
-
-?>
