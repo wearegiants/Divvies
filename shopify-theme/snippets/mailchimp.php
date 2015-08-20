0d807a47a77dc1ad316cf79ca755015d402b6aa6
Craig Fowler <craig@wearegiants.com>
2015-08-19 18:53:32 -0700

    added divvies mailchimp info

--- shopify-theme/snippets/mailchimp.php
+++ shopify-theme/snippets/mailchimp.php
@@ -1,3 +1,18 @@
 <div id="mailchimp">
-  <button class="btn">Sign up for the Newsletter</button>
+   <button class="btn">Sign up for the Newsletter</button>
+</div>
+
+<div id="mailchimp-popup">
+   <div id="mailchimp-popup-wrapper">
+      <div id="mc_embed_signup">
+         <form action="//divvies.us1.list-manage.com/subscribe/post?u=9526e58f22c750b09cd234943&amp;id=467716618f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+               <label for="mce-EMAIL">Subscribe to our mailing list</label>
+               <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+               <div style="position: absolute; left: -5000px;"><input type="text" name="b_9526e58f22c750b09cd234943_467716618f" tabindex="-1" value=""></div>
+               <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button btn bg-color-orange">
+            </div>
+         </form>
+      </div>
+   </div>
 </div>
